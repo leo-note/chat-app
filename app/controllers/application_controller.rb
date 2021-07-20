89316194e5f38523ca0bbd4085572b1580888d66
leo-note <karen.koizumi7@gmail.com>
2021-07-20 16:09:45 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :name_save, if: :devise_controller?
+
+  private
+  def name_save
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
